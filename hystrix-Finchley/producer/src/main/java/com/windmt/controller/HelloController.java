c32b792b93f88262b31b2120459920319a6bb57c
Zhao Yibo <zhaoyibochn@gmaill.com>
2018-04-18 10:34:07 +0800

    update

--- hystrix-Finchley/producer/src/main/java/com/windmt/controller/HelloController.java
+++ hystrix-Finchley/producer/src/main/java/com/windmt/controller/HelloController.java
@@ -15,7 +15,7 @@ public class HelloController {
 
     @GetMapping("")
     public String hello(@RequestParam String name) {
-        return "Hello, " + name;
+        return "Hello, " + name + "!";
     }
 
 }
