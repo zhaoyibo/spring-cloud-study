8dc36dc4bcf45ce06d0d3922a615e2f3fe652246
Zhao Yibo <zhaoyibochn@gmaill.com>
2018-04-19 11:58:30 +0800

    config-git

---
+++ config-git/config-client/src/main/java/com/windmt/controller/HelloController.java
@@ -0,0 +1,22 @@
+package com.windmt.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
+
+/**
+ * @author: zhaoyibochn@gmail.com
+ * @create: 2018-04-19
+ */
+@RestController
+public class HelloController {
+
+    @Value("${info.profile:error}")
+    private String profile;
+
+    @GetMapping("/info")
+    public Mono<String> hello() {
+        return Mono.justOrEmpty(profile);
+    }
+}
