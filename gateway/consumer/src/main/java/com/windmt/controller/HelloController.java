6b9b3176345b4a4608f157e3851bb13859458be0
Zhao Yibo <zhaoyibochn@gmaill.com>
2018-05-11 11:50:21 +0800

    retry

--- gateway/consumer/src/main/java/com/windmt/controller/HelloController.java
+++ gateway/consumer/src/main/java/com/windmt/controller/HelloController.java
@@ -1,6 +1,7 @@
 package com.windmt.controller;
 
 import com.windmt.feign.HelloRemote;
+import lombok.extern.apachecommons.CommonsLog;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -8,10 +9,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Date;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @author: yibo
  **/
+@CommonsLog
 @RequestMapping("/hello")
 @RestController
 public class HelloController {
@@ -20,9 +23,13 @@ public class HelloController {
     HelloRemote helloRemote;
 
     @GetMapping("/{name}")
-    public String index(@PathVariable("name") String name) {
-        if ("null".equals(name)) {
-            throw new NullPointerException();
+    public String index(@PathVariable("name") String name) throws InterruptedException {
+        log.info("the name is " + name);
+        switch (name) {
+            case "null":
+                throw new NullPointerException();
+            case "retry":
+                TimeUnit.MINUTES.sleep(10);
         }
         return helloRemote.hello(name) + "\n" + new Date().toString();
     }
