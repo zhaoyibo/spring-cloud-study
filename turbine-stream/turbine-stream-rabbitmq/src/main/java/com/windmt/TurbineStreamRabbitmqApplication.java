eabcb478277a73456f219320de04fd42f069a7a4
Zhao Yibo <zhaoyibochn@gmaill.com>
2018-06-01 09:59:03 +0800

    spring cloud RC2

--- turbine-stream/turbine-stream-rabbitmq/src/main/java/com/windmt/TurbineStreamRabbitmqApplication.java
+++ turbine-stream/turbine-stream-rabbitmq/src/main/java/com/windmt/TurbineStreamRabbitmqApplication.java
@@ -3,9 +3,6 @@ package com.windmt;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.turbine.stream.EnableTurbineStream;
-import org.springframework.cloud.stream.converter.CompositeMessageConverterFactory;
-import org.springframework.context.annotation.Bean;
-import org.springframework.integration.support.converter.ConfigurableCompositeMessageConverter;
 
 /**
  * @author yibo
@@ -18,8 +15,4 @@ public class TurbineStreamRabbitmqApplication {
         SpringApplication.run(TurbineStreamRabbitmqApplication.class, args);
     }
 
-    @Bean
-    public ConfigurableCompositeMessageConverter integrationArgumentResolverMessageConverter(CompositeMessageConverterFactory factory) {
-        return new ConfigurableCompositeMessageConverter(factory.getMessageConverterForAllRegistered().getConverters());
-    }
 }
